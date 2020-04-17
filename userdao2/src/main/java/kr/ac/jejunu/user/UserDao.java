92c6adc495980241438368aa89b01d2bdd30e7aa
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2020-04-17 19:25:55 +0900

    0417-part2실습
    part2실습내용입니다.

--- userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -2,7 +2,7 @@ package kr.ac.jejunu.user;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
         //mysql driver 로딩
         Connection connection = getConnection();
@@ -40,10 +40,10 @@ public class UserDao {
         connection.close();
     }
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        //mysql driver 로딩
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        //connection 맺기
-        return DriverManager.getConnection("jdbc:mysql://localhost/userdao?serverTimezone=Asia/Seoul", "root", "as1212");
-    }
+    abstract public Connection getConnection() throws ClassNotFoundException, SQLException;
+//        //mysql driver 로딩
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        //connection 맺기
+//        return DriverManager.getConnection("jdbc:mysql://localhost/userdao?serverTimezone=Asia/Seoul", "root", "as1212");
+
 }
