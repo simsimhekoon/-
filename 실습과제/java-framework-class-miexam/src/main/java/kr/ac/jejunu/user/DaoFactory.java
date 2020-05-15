7f9637faab40543fde810080013b056430702cbd
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2020-05-15 22:38:48 +0900

    실습과제13번
    각자 생성한 Context 를 JdbcTemplate 으로 변경해보세요.
    add 의 경우 상품정보에서도 활용이 가능합니다 JejuJdbcTemplate 을 만들어보세요

--- 실습과제/java-framework-class-miexam/src/main/java/kr/ac/jejunu/user/DaoFactory.java
+++ 실습과제/java-framework-class-miexam/src/main/java/kr/ac/jejunu/user/DaoFactory.java
@@ -5,6 +5,7 @@ import kr.ac.jejunu.userdao.UserDao;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
@@ -27,8 +28,8 @@ public class DaoFactory {
     }
 
     @Bean
-    public JdbcContext jdbcContext() {
-        return new JdbcContext(dataSource());
+    public JdbcTemplate jdbcContext() {
+        return new JdbcTemplate(dataSource());
     }
 
     @Bean
