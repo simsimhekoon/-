6e56161422da01a3be0532c987f71c30d8c510b8
simsimhekoon <37258541+simsimhekoon@users.noreply.github.com>
2020-05-15 19:22:20 +0900

    실습과제3번
    상품정보 관리 요구사항이 나왔습니다. Connection을 상품정보에도 사용할 수 있도록
    추상화하시오 ( Strategy Pattern)

--- 실습과제/java-framework-class-miexam/src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ 실습과제/java-framework-class-miexam/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -2,9 +2,15 @@ package kr.ac.jejunu.userdao;
 
 import java.sql.*;
 
-public abstract class UserDao {
+public class UserDao {
+    private final ConnectionMaker connectionMaker;
+
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
     public User get(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
+        Connection connection = connectionMaker.getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select * from userinfo where id = ?");
         preparedStatement.setLong(1, id);
@@ -25,7 +31,7 @@ public abstract class UserDao {
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
+        Connection connection = connectionMaker.getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("insert into userinfo (name, password) values(?, ?)", Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, user.getName());
@@ -38,9 +44,4 @@ public abstract class UserDao {
         preparedStatement.close();
         connection.close();
     }
-
-    abstract public Connection getConnection() throws ClassNotFoundException, SQLException ;
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        return DriverManager.getConnection("jdbc:mysql://localhost/userdao?serverTimezone=UTC"
-//                , "root", "as1212");
 }
--- 실습과제/java-framework-class-miexam/src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ 실습과제/java-framework-class-miexam/src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -17,7 +17,8 @@ public class UserDaoTests {
     public void get() throws SQLException, ClassNotFoundException {
         Integer id = 1;
 
-        UserDao userDao = new JejuUserDao();
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -29,36 +30,8 @@ public class UserDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        UserDao userDao = new JejuUserDao();
-        userDao.insert(user);
-        assertThat(user.getId(), greaterThan(0));
-
-        User insertedUser = userDao.get(user.getId());
-        assertThat(insertedUser.getName(), is(name));
-        assertThat(insertedUser.getPassword(), is(password));
-    }
-
-
-
-
-
-    @Test
-    public void getHalla() throws SQLException, ClassNotFoundException {
-        Integer id = 1;
-
-        UserDao userDao = new HallaUserDao();
-        User user = userDao.get(id);
-        assertThat(user.getId(), is(id));
-        assertThat(user.getName(), is(name));
-        assertThat(user.getPassword(), is(password));
-    }
-
-    @Test
-    public void insertHalla() throws SQLException, ClassNotFoundException {
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-        UserDao userDao = new HallaUserDao();
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
         userDao.insert(user);
         assertThat(user.getId(), greaterThan(0));
 
